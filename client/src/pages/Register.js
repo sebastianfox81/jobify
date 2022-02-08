870f8a1e8748d8ad91c5cba00686471d0664c6a5
Sebastian <sebastianfox81@gmail.com>
2022-02-08 13:38:07 -0500

    Created form in Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true
+}
 
 const Register = () => {
+
+  const [ values, setValues ] = useState(initialState);
+  // global state and useNavigate
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const handleSubmit  = (e) => {
+    e.preventDefault();
+    console.log(e.target)
+  }
+
   return <div>
-    <h1>Register</h1>
+    <h3>Register</h3>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name"></label>
+      <input type="text" id="name" onChange={handleChange}/>
+      <label htmlFor="email"></label>
+      <input type="text" id="email" onChange={handleChange}/>
+      <label htmlFor="password"></label>
+      <input type="text" id="password" onChange={handleChange}/>
+      <button type="submit">Submit</button>
+    </form>
   </div>;
 };
 
