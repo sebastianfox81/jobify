ee632079ad1355d4fd6fa2fc8e595e89969ecf16
Sebastian <sebastianfox81@gmail.com>
2022-02-09 12:56:36 -0500

    Created password hash functionality in User schema model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const validator = require('validator')
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -39,6 +40,11 @@ const userSchema = new mongoose.Schema({
   timestamps: true
 });
 
+userSchema.pre('save', async function() {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt)
+  console.log(this.password)
+})
 
 const User = mongoose.model('User', userSchema);
 
