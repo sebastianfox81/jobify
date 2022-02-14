6ca41372cb6d652b5a988fc8a6d47b68e183b5ea
Sebastian <sebastianfox81@gmail.com>
2022-02-14 16:43:44 -0500

    Added links to navbar component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaHome } from 'react-icons/fa';
 import { useGlobalContext } from '../context/appContext';
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -9,6 +10,10 @@ const Navbar = () => {
   return (
     <div>
       <h2>Navbar</h2>
+      <Link to='/'>Stats</Link>
+      <Link to='/add-job'>Add Job</Link>
+      <Link to='/all-jobs'>All Jobs</Link>
+      <Link to='/profile'>My Profile</Link>
       <button onClick={logoutUser}><h5>Logout</h5></button>
       <FaHome />
     </div>
