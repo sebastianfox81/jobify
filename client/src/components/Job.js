818998c5a7c1b806e8328745ebd294eb868eb5e9
Sebastian <sebastianfox81@gmail.com>
2022-02-17 22:57:26 -0500

    Added edit/delete placeholder functions to Job component

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,10 +1,56 @@
-import React from 'react'
+import moment from 'moment'
+import { Link } from 'react-router-dom'
+import { useGlobalContext } from '../context/appContext'
+// import JobInfo from './JobInfo'
 
-const Job = ({ company, position, }) => {
+const Job = ({
+  _id,
+  position,
+  company,
+  jobLocation,
+  jobType,
+  createdAt,
+  statusType,
+}) => {
+  const { setEditJob, deleteJob } = useGlobalContext()
+
+  let date = moment(createdAt)
+  date = date.format('MMM Do, YYYY')
   return (
     <div>
-      <h5>{company}</h5>
-      
+      <header>
+        <div className='main-icon'>{company.charAt(0)}</div>
+        <div className='info'>
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className='content'>
+        <div className='content-center'>
+          {jobLocation}
+          {date}
+          {jobType}
+          <div className={`status ${statusType}`}>{statusType}</div>
+        </div>
+        <footer>
+          <div className='actions'>
+            <Link
+              to='/add-job'
+              className='btn edit-btn'
+              onClick={() => setEditJob(_id)}
+            >
+              Edit
+            </Link>
+            <button
+              type='button'
+              className='btn delete-btn'
+              onClick={() => deleteJob(_id)}
+            >
+              Delete
+            </button>
+          </div>
+        </footer>
+      </div>
     </div>
   )
 }
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,10 +1,10 @@
 import React, { useContext, useReducer, useEffect } from "react";
 import reducer from "./reducer";
-import axios from 'axios';
+import axios from "axios";
 
-const user = localStorage.getItem('user')
-const token = localStorage.getItem('token')
-const userLocation = localStorage.getItem('location')
+const user = localStorage.getItem("user");
+const token = localStorage.getItem("token");
+const userLocation = localStorage.getItem("location");
 
 export const initialState = {
   isLoading: false,
@@ -13,20 +13,20 @@ export const initialState = {
   alertType: "",
   user: user ? JSON.parse(user) : null,
   token: token,
-  userLocation: userLocation || '',
+  userLocation: userLocation || "",
   isEditing: false,
-  editJobId: '',
-  position: '',
-  company: '',
-  jobLocation: userLocation || '',
-  jobTypeOptions: ['full time', 'part time', 'remote', 'internship'],
-  jobType: 'full time',
-  statusOptions: ['interview', 'declined', 'pending'],
-  statusType: 'pending',
+  editJobId: "",
+  position: "",
+  company: "",
+  jobLocation: userLocation || "",
+  jobTypeOptions: ["full time", "part time", "remote", "internship"],
+  jobType: "full time",
+  statusOptions: ["interview", "declined", "pending"],
+  statusType: "pending",
   jobs: [],
   totalJobs: 0,
   numOfPages: 1,
-  page: 1
+  page: 1,
 };
 
 const AppContext = React.createContext();
@@ -35,142 +35,171 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const displayAlert = () => {
-    dispatch({ type: 'SHOW_ALERT' });
-    clearAlert()
+    dispatch({ type: "SHOW_ALERT" });
+    clearAlert();
   };
 
   const clearAlert = () => {
     setTimeout(() => {
-      dispatch({ type: 'CLEAR_ALERT'})
-    }, 3000)
-  }
+      dispatch({ type: "CLEAR_ALERT" });
+    }, 3000);
+  };
 
   const addUserToLocalStorage = ({ user, token, location }) => {
-    localStorage.setItem('user', JSON.stringify(user))
-    localStorage.setItem('token', token)
-    localStorage.setItem('location', userLocation)
-  }
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    localStorage.setItem("location", userLocation);
+  };
 
   const removeUserFromLocalStorage = () => {
-    localStorage.removeItem('user')
-    localStorage.removeItem('token')
-    localStorage.removeItem('location')
-  }
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("location");
+  };
 
   const registerUser = async (currUser) => {
-    dispatch({ type: 'REGISTER_USER_BEGIN'})
+    dispatch({ type: "REGISTER_USER_BEGIN" });
     try {
-      const response = await axios.post('/api/v1/auth/register', currUser)
-      console.log(response)
-      const { user, token, location } = response.data
+      const response = await axios.post("/api/v1/auth/register", currUser);
+      console.log(response);
+      const { user, token, location } = response.data;
       dispatch({
-        type: 'REGISTER_USER_SUCCESS',
+        type: "REGISTER_USER_SUCCESS",
         payload: { user, token, location },
-      })
-      addUserToLocalStorage({ user, token, location })
+      });
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      dispatch({ type: 'REGISTER_USER_ERROR', payload: { msg: error.response.data.msg}})
-      console.log(error.response.data.msg)
+      dispatch({
+        type: "REGISTER_USER_ERROR",
+        payload: { msg: error.response.data.msg },
+      });
+      console.log(error.response.data.msg);
     }
     // console.log(currUser)
-    clearAlert()
-  }
+    clearAlert();
+  };
 
   const loginUser = async (currUser) => {
-    dispatch({ type: 'LOGIN_USER_BEGIN'})
+    dispatch({ type: "LOGIN_USER_BEGIN" });
     try {
-      const response = await axios.post('/api/v1/auth/login', currUser)
-      console.log(response)
-      const { user, token, location } = response.data
+      const response = await axios.post("/api/v1/auth/login", currUser);
+      console.log(response);
+      const { user, token, location } = response.data;
       dispatch({
-        type: 'LOGIN_USER_SUCCESS',
+        type: "LOGIN_USER_SUCCESS",
         payload: { user, token, location },
-      })
-      addUserToLocalStorage({ user, token, location })
+      });
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      dispatch({ type: 'LOGIN_USER_ERROR', payload: { msg: error.response.data.msg}})
-      console.log(error.response.data.msg)
+      dispatch({
+        type: "LOGIN_USER_ERROR",
+        payload: { msg: error.response.data.msg },
+      });
+      console.log(error.response.data.msg);
     }
     // console.log(currUser)
-    clearAlert()
-  }
+    clearAlert();
+  };
 
   const logoutUser = () => {
-    dispatch({ type: 'LOGOUT_USER'});
+    dispatch({ type: "LOGOUT_USER" });
     removeUserFromLocalStorage();
-  }
+  };
 
   const updateUser = async (id, currUser) => {
-    dispatch({ type: 'UPDATE_USER_BEGIN'})
+    dispatch({ type: "UPDATE_USER_BEGIN" });
     try {
-      const res = await axios.patch(`/api/v1/auth/updateUser/${id}`, currUser)
+      const res = await axios.patch(`/api/v1/auth/updateUser/${id}`, currUser);
 
-      const { user, location, token} = res.data;
+      const { user, location, token } = res.data;
 
       dispatch({
-        type: 'UPDATE_USER_SUCCESS',
+        type: "UPDATE_USER_SUCCESS",
         payload: { user, location, token },
-
       });
-      addUserToLocalStorage({ user, location, token })
+      addUserToLocalStorage({ user, location, token });
     } catch (error) {
       dispatch({
-        type: 'UPDATE_USER_ERROR',
-        payload: { msg: error.response.data.msg},
-      })
+        type: "UPDATE_USER_ERROR",
+        payload: { msg: error.response.data.msg },
+      });
     }
-    clearAlert()
-  }
+    clearAlert();
+  };
 
-  const handleJobChange = ({ name, value}) => {
-    dispatch({ type: 'HANDLE_JOB_CHANGE', payload: { name, value}})
-  }
+  const handleJobChange = ({ name, value }) => {
+    dispatch({ type: "HANDLE_JOB_CHANGE", payload: { name, value } });
+  };
 
   const clearValues = () => {
-    dispatch({ type: 'CLEAR_VALUES'})
-  }
+    dispatch({ type: "CLEAR_VALUES" });
+  };
 
   const createJob = async () => {
-    dispatch({ type: 'CREATE_JOB_BEGIN'});
+    dispatch({ type: "CREATE_JOB_BEGIN" });
     try {
       const id = state.user._id;
-      console.log(id)
+      console.log(id);
       const { position, company, jobLocation, jobType, statusType } = state;
-      const job = { position, company, jobLocation, jobType, statusType }
+      const job = { position, company, jobLocation, jobType, statusType };
       await axios.post(`/api/v1/jobs/${id}`, job);
-      dispatch({ type: 'CREATE_JOB_SUCCESS'})
-      dispatch({ type: 'CLEAR_VALUES'})
+      dispatch({ type: "CREATE_JOB_SUCCESS" });
+      dispatch({ type: "CLEAR_VALUES" });
     } catch (error) {
       if (error.response.status === 401) return;
-      dispatch({ type: 'CREATE_JOB_ERROR', payload: { msg: error.response.data.msg }})
-    }
-    clearAlert()
-  }
-  const getAllJobs = async () => {
-    dispatch({ type: 'GET_JOBS_BEGIN'});
-    try {
-      const id = state.user._id
-      const res = await axios.get(`api/v1/jobs/${id}`)
-      const { jobs, totalJobs, numOfPages } = res.data
       dispatch({
-        type: 'GET_JOBS_SUCCESS',
+        type: "CREATE_JOB_ERROR",
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
+  };
+  const getAllJobs = async () => {
+    dispatch({ type: "GET_JOBS_BEGIN" });
+    try {
+      const id = state.user._id;
+      const res = await axios.get(`api/v1/jobs/${id}`);
+      const { jobs, totalJobs, numOfPages } = res.data;
+      dispatch({
+        type: "GET_JOBS_SUCCESS",
         payload: {
           jobs,
           totalJobs,
-          numOfPages
-        }
-      })
+          numOfPages,
+        },
+      });
       clearAlert();
-      console.log(res.data.jobs)
+      console.log(res.data.jobs);
     } catch (error) {
-      console.log(error.response)
+      console.log(error.response);
       // logoutUser();
     }
-  }
 
+  };
+  const setEditJob = (id) => {
+    console.log(`set edit job: ${id}`);
+  };
+  const deleteJob = (id) => {
+    console.log(`delete job: ${id}`);
+  };
 
   return (
-    <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser, logoutUser, updateUser, handleJobChange, clearValues, createJob, getAllJobs }}>
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        registerUser,
+        loginUser,
+        logoutUser,
+        updateUser,
+        handleJobChange,
+        clearValues,
+        createJob,
+        getAllJobs,
+        setEditJob,
+        deleteJob
+      }}
+    >
       {children}
     </AppContext.Provider>
   );
