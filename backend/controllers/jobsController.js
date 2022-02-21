41085f538d91ee00ec44f0d662873a7551e48f83
Sebastian <sebastianfox81@gmail.com>
2022-02-21 12:13:08 -0500

    Implemented pagination functionality in the backend

--- backend/controllers/jobsController.js
+++ backend/controllers/jobsController.js
@@ -50,8 +50,21 @@ const getAllJobs = async (req, res) => {
   if (sort === 'z-a') {
     result = result.sort('-position')
   }
+
+  // Pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
+
   const jobs = await result
- res.status(statusCodes.default.OK).json({ jobs, totalJobs: jobs.length, numOfPages: 1})
+
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs/limit)
+
+ res.status(statusCodes.default.OK).json({ jobs, totalJobs, numOfPages})
 
 }
 
